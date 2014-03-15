0860a7e5be66dd687f5b3ab78fd4c39403a1b2d2
Dahua Lin <lindahua@gmail.com>
2014-03-15 13:10:47 -0500

    test equality of variables

--- test/common.jl
+++ test/common.jl
@@ -15,15 +15,25 @@ v = Constant(2.5)
 @test eltype(v) == Float64
 @test ndims(v) == 0
 
+@test v == Constant(2.5)
+@test v != Constant(3.0)
+
 v = ScalarVar(:x, Int32)
 @test isa(v, ScalarVar)
 @test v.sym == :x
 @test eltype(v) == Int32
 @test ndims(v) == 0
 
+@test v == ScalarVar(:x, Int32)
+@test v != ScalarVar(:y, Int32)
+
+
 v = ArrayVar(:a, Float32, 3)
 @test isa(v, ArrayVar)
 @test v.sym == :a
 @test eltype(v) == Float32
 @test ndims(v) == 3
+
+@test v == ArrayVar(:a, Float32, 3)
+@test v != ArrayVar(:a, Float32, 2)
 
